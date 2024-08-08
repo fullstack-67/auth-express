bbcceaffff39f41d0ba5c7ab987d89126f819e7d
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2024-08-08 17:16:43 +0700

    Update

---
+++ src/index.ts
@@ -0,0 +1,51 @@
+import "dotenv/config";
+import express, { ErrorRequestHandler } from "express";
+import cors from "cors";
+import helmet from "helmet";
+import passport from "passport";
+import session from "express-session";
+import { Strategy as LocalStrategy } from "passport-local";
+//Intializing the express app
+const app = express();
+
+//Middleware
+app.use(helmet());
+app.use(
+  cors({
+    origin: false, // Disable CORS
+    // origin: "*", // Allow all origins
+  })
+);
+app.use(
+  session({
+    secret: "123456",
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+// Extracts the entire body portion of an incoming request stream and exposes it on req.body.
+app.use(express.json());
+
+passport.use(
+  new LocalStrategy(
+    { usernameField: "username", passwordField: "password" },
+    function (username, password, done) {
+      return done(null, true);
+    }
+  )
+);
+
+app.get("/", async (req, res, next) => {
+  res.send("Hello");
+});
+
+app.post("/api/login", passport.authenticate("local"), function (req, res) {
+  res.json(req.user);
+});
+
+// Running app
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, async () => {
+  console.log(`Listening on port ${PORT}`);
+  console.log(`http://localhost:${PORT}`);
+});
