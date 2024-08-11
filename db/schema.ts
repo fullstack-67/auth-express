6d96caa0c61e66646c10bec7e987a794b3c09a49
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2024-08-11 17:37:01 +0700

    Update

--- db/schema.ts
+++ db/schema.ts
@@ -1,16 +1,17 @@
-import { sql } from "drizzle-orm";
 import { text, integer, sqliteTable } from "drizzle-orm/sqlite-core";
-import * as nanoid from "nanoid";
+import { nanoid } from "nanoid";
 
 export const usersTable = sqliteTable("users", {
   id: text("id")
     .primaryKey()
-    .$defaultFn(() => nanoid.nanoid()),
+    .$defaultFn(() => nanoid()),
   name: text("name"),
-  createdAt: text("created_at")
-    .notNull()
-    .default(sql`CURRENT_TIMESTAMP`),
+  email: text("email").unique().notNull(),
+  password: text("password").notNull(),
   isAdmin: integer("is_admin", { mode: "boolean" }).notNull().default(false),
+  createdAt: integer("created_at", { mode: "timestamp_ms" }).$default(
+    () => new Date()
+  ),
 });
 
 export const sessionsTable = sqliteTable("sessions", {
--- db/seed.ts
+++ db/seed.ts
@@ -1,17 +1,28 @@
 import { dbClient, dbConn } from "@db/client";
 import { usersTable } from "@db/schema";
+import bcrypt from "bcrypt";
+
+const saltRounds = 10;
+const password = "1234";
+
 async function insertData() {
-  await dbClient.insert(usersTable).values([
-    {
-      name: "Admin User",
-      isAdmin: true,
-    },
-    {
-      name: "Regular User",
-      isAdmin: false,
-    },
-  ]);
-  dbConn.close();
+  bcrypt.hash(password, saltRounds, async function (err, hash) {
+    await dbClient.insert(usersTable).values([
+      {
+        name: "Admin User",
+        email: "admin@cmu.com",
+        isAdmin: true,
+        password: hash,
+      },
+      {
+        name: "Regular User",
+        email: "user@cmu.com",
+        isAdmin: false,
+        password: hash,
+      },
+    ]);
+    dbConn.close();
+  });
 }
 
 insertData();
