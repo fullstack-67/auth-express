d0c01f50d9933c712e959828666ad1558593e04d
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2024-08-15 07:46:37 +0700

    Update

--- src/passportStratOauth.ts
+++ src/passportStratOauth.ts
@@ -3,6 +3,7 @@ import {
   type VerifyCallback,
 } from "passport-oauth2";
 import axios from "axios";
+import { GithubUser, GithubEmails } from "./types/github";
 
 import {
   clientID,
@@ -23,15 +24,39 @@ export const github = new OAuthStrategy(
     passReqToCallback: false,
   },
   // I have to manually annotate this from d.ts file. This should not be the case.
-  function (
+  async function (
     accessToken: string,
     refreshToken: string,
     profile: any,
     done: VerifyCallback
   ) {
-    console.log({ accessToken, refreshToken, profile, done });
+    // console.log({ accessToken, refreshToken, profile, done });
 
-    // axios.get()
+    const resUser = await axios.request<GithubUser>({
+      method: "GET",
+      url: "https://api.github.com/user",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    const resEmails = await axios.request<GithubEmails>({
+      method: "GET",
+      url: "https://api.github.com/user/emails",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    console.log({ resUser: resUser.data, resEmails: resEmails.data });
+
+    const email = resEmails.data.find((em) => em.primary) ?? null;
+    if (!email) done("Cannot find email", false);
+
+    const u = resUser.data;
+    const providerID = u.node_id;
+    const avatarURl = u.avatar_url;
+    const name = u.name;
+
     // User.findOrCreate({ exampleId: profile.id }, function (err, user) {
     //   return cb(err, user);
     // });
