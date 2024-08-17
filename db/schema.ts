787a8716153bb723a2701fcc516e9edd3d358332
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2024-08-17 08:26:39 +0700

    Update

--- db/schema.ts
+++ db/schema.ts
@@ -1,5 +1,11 @@
-import { text, integer, sqliteTable } from "drizzle-orm/sqlite-core";
+import {
+  text,
+  integer,
+  sqliteTable,
+  primaryKey,
+} from "drizzle-orm/sqlite-core";
 import { nanoid } from "nanoid";
+import { relations } from "drizzle-orm";
 
 export const usersTable = sqliteTable("users", {
   id: text("id")
@@ -7,12 +13,43 @@ export const usersTable = sqliteTable("users", {
     .$defaultFn(() => nanoid()),
   name: text("name"),
   email: text("email").unique().notNull(),
-  password: text("password").notNull(),
+  password: text("password"), // I remove not null options.
   isAdmin: integer("is_admin", { mode: "boolean" }).notNull().default(false),
   createdAt: integer("created_at", { mode: "timestamp_ms" }).$default(
     () => new Date()
   ),
 });
+
+export const usersRelations = relations(usersTable, ({ many }) => ({
+  accounts: many(accountsTable),
+}));
+
+export const accountsTable = sqliteTable(
+  "accounts",
+  {
+    id: text("id")
+      .$defaultFn(() => nanoid())
+      .notNull()
+      .unique(),
+    userId: text("user_id").notNull(),
+    provider: text("provider", { enum: ["GITHUB", "DISCORD"] }).notNull(),
+    providerAccountId: text("provider_account").notNull(),
+    profile: text("profile", { mode: "json" }),
+  },
+  // I add composite key so that each user can have only one provider type.
+  (table) => {
+    return {
+      id: primaryKey({ columns: [table.userId, table.provider] }),
+    };
+  }
+);
+
+export const accountsRelations = relations(accountsTable, ({ one }) => ({
+  user: one(usersTable, {
+    fields: [accountsTable.userId],
+    references: [usersTable.id],
+  }),
+}));
 
 export const sessionsTable = sqliteTable("sessions", {
   sid: text("sid").primaryKey(),
--- db/seed.ts
+++ db/seed.ts
@@ -1,5 +1,5 @@
 import { dbClient, dbConn } from "@db/client";
-import { usersTable } from "@db/schema";
+import { usersTable, accountsTable } from "@db/schema";
 import bcrypt from "bcrypt";
 
 const saltRounds = 10;
@@ -7,20 +7,26 @@ const password = "1234";
 
 async function insertData() {
   bcrypt.hash(password, saltRounds, async function (err, hash) {
-    await dbClient.insert(usersTable).values([
-      {
-        name: "Admin User",
-        email: "admin@cmu.com",
-        isAdmin: true,
-        password: hash,
-      },
-      {
-        name: "Regular User",
-        email: "user@cmu.com",
-        isAdmin: false,
-        password: hash,
-      },
-    ]);
+    const results = await dbClient
+      .insert(usersTable)
+      .values([
+        {
+          name: "Admin User",
+          email: "admin@cmu.com",
+          isAdmin: true,
+          password: hash,
+        },
+        {
+          name: "Regular User",
+          email: "user@cmu.com",
+          isAdmin: false,
+          password: hash,
+        },
+      ])
+      .returning({ id: usersTable.id });
+
+    console.log(results);
+
     dbConn.close();
   });
 }
